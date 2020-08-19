b517d1fb6ffd571d9d79a3a5ecaca314c9f7bd76
David Soards <davidysoards@gmail.com>
2020-08-19 15:53:56 -0400

    add AIGA logo to footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,13 +1,22 @@
 <template>
-  <footer class="bg-black">
-    <div class="p-4 max-w-screen-xl mx-auto text-sm text-white">
+  <footer class="bg-black p-10">
+    <div class="max-w-screen-lg mx-auto text-sm text-white lg:flex justify-between">
+      <a href="//aiga.org" target="_blank" rel="noopener noreferrer">
+        <div class="w-24 mb-8 lg:mb-0">
+          <svg-aiga-full-logo />
+        </div>
+      </a>
       <p>© {{ year }} AIGA Midwest Design Week</p>
     </div>
   </footer>
 </template>
 
 <script>
+import SvgAigaFullLogo from '@/components/SvgAigaFullLogo';
 export default {
+  components: {
+    SvgAigaFullLogo,
+  },
   data() {
     return {
       year: new Date().getFullYear(),
