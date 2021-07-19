08ddd1d6695350fb436c727691c8d5b91006ccd3
David Soards <davidysoards@gmail.com>
2021-07-19 17:12:31 -0400

    Use tailwind JIT compiler

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.js', 'nuxt.config.js'],
   darkMode: false,
   theme: {
