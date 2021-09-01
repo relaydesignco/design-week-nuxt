ce3ace486777e2a25e0422c4f843577131d9df02
David Soards <davidysoards@gmail.com>
2021-08-31 22:51:32 -0400

    remove event iframe until before event

--- pages/mainevent.vue
+++ pages/mainevent.vue
@@ -2,7 +2,7 @@
   <div class="pt-20 lg:pt-28 px-4 lg:px-8 pb-8">
     <div class="container mx-auto">
       <h1 class="text-2xl lg:text-3xl mb-8 lg:mb-12 uppercase text-center tracking-wide">Welcome to the Main Event</h1>
-      <div class="aspect-w-16 aspect-h-9 w-full">
+      <!-- <div class="aspect-w-16 aspect-h-9 w-full">
         <iframe
           width="1280"
           height="720"
@@ -11,7 +11,7 @@
           encrypted-media
           allowfullscreen
         ></iframe>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
